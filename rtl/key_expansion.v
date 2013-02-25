2aac79cf7d5e3cb84775d2f1695031904881d370
Florian <fraudies@bu.edu>
2013-02-25 09:41:44 -0500

    Revert "For now, 5XOR and one memory access as longest path."
    This reverts commit 9074da4f6060a2185759aad72042d30d1f0b2b0f.

--- rtl/key_expansion.v
+++ rtl/key_expansion.v
@@ -9,20 +9,20 @@ module key_expansion(
 	input 			key_in_valid, 	/* valid input key at key_in */
 	input  [127:0] key_in, 			/* input key I ASSUME bit0 is the first bit received and bit 128 the last one.*/
 	output [127:0] key_out, 		/* output to register's in Ben's design */
-	output [3:0] 	key_addr,		/* 1,...,10 for 10 keys, 0 nonvalid addr */
+	output [3:0] 	key_addr,		/* 1,...,11 for 11 keys, 0 nonvalid addr */
 	output 			key_loaded); 	/* key completely loaded */
 	
 	localparam READY		= 4'd0,
-				  ROUND_1	= 4'd1,
-				  ROUND_2 	= 4'd2,
-				  ROUND_3 	= 4'd3,
-				  ROUND_4 	= 4'd4,
-				  ROUND_5 	= 4'd5,
-				  ROUND_6 	= 4'd6,
-				  ROUND_7 	= 4'd7,
-				  ROUND_8 	= 4'd8,
-				  ROUND_9 	= 4'd9,
-				  ROUND_10 	= 4'd10,
+				  INIT 		= 4'd1,
+				  ROUND_1 	= 4'd2,
+				  ROUND_2 	= 4'd3,
+				  ROUND_3 	= 4'd4,
+				  ROUND_4 	= 4'd5,
+				  ROUND_5 	= 4'd6,
+				  ROUND_6 	= 4'd7,
+				  ROUND_7 	= 4'd8,
+				  ROUND_8 	= 4'd9,
+				  ROUND_9 	= 4'd10,
 				  DONE 		= 4'd11;
 	
 	/* four bits for state */
@@ -48,57 +48,58 @@ module key_expansion(
 		rcon_addr 	= 4'd0; /* note that rcon_addr is not always key_addr - 1 */
 		case (state)
 			READY : if (key_in_valid) begin
-						next 			= ROUND_1;
+						next 			= INIT;
 						load_enable = 'b1;
 					  end else 				
 						next 			= READY;
-			ROUND_1 : begin 							  /* B0 B1 B2 B3 all set */
-						next 			= ROUND_2; 	 /* 1st key ready */
-						key_addr 	= 4'd1;
+			INIT : begin 							  /* B0 B1 B2 B3 all set */
+						next 			= ROUND_1; 	 /* 1st key ready */
+						key_addr 	= 4'd1; 			  
 					 end
-			ROUND_2 : begin
-							next 			= ROUND_3; /* 2nd key ready */
+			ROUND_1 : begin
+							next 			= ROUND_2; /* 2nd key ready */
 							key_addr 	= 4'd2;
 							rcon_addr 	= 4'd1;
 					    end
-			ROUND_3 : begin
-							next 			= ROUND_4; /* 3rd key ready */
+			ROUND_2 : begin
+							next 			= ROUND_3; /* 3rd key ready */
 							key_addr 	= 4'd3;
 							rcon_addr 	= 4'd2;
 					 	 end
-			ROUND_4 : begin
-							next 			= ROUND_5; /* 4th key ready */
+			ROUND_3 : begin
+							next 			= ROUND_4; /* 4th key ready */
 							key_addr 	= 4'd4;
 							rcon_addr 	= 4'd3;
 						 end
-			ROUND_5 : begin
-							next 			= ROUND_6; /* 5th key ready */
+			ROUND_4 : begin
+							next 			= ROUND_5; /* 5th key ready */
 							key_addr 	= 4'd5;
 							rcon_addr 	= 4'd4;
 						end
-			ROUND_6 : begin
-							next 			= ROUND_7; /* 6th key ready */
+			ROUND_5 : begin
+							next 			= ROUND_6; /* 6th key ready */
 							key_addr 	= 4'd6;
 							rcon_addr 	= 4'd5;
 						end
-			ROUND_7 : begin
-							next 			= ROUND_8; /* 7th key ready */
+			ROUND_6 : begin
+							next 			= ROUND_7; /* 7th key ready */
 							key_addr 	= 4'd7;
 							rcon_addr 	= 4'd6;
 						end
-			ROUND_8 : begin
-							next 			= ROUND_9; /* 8th key ready */
+			ROUND_7 : begin
+							next 			= ROUND_8; /* 8th key ready */
 							key_addr 	= 4'd8;
 							rcon_addr 	= 4'd7;
 						end
-			ROUND_9 : begin
-							next 			= ROUND_10; /* 9th key ready */
+			ROUND_8 : begin
+							next 			= ROUND_9; /* 9th key ready */
 							key_addr 	= 4'd9;
 							rcon_addr 	= 4'd8;
 						end
-			ROUND_10 : begin
+			ROUND_9 : begin
 							next 			= DONE; /* 10th key ready */
 							key_addr 	= 4'd10;
+							rcon_addr 	= 4'd9;
 						end
 			DONE : begin
 						next = DONE;
@@ -111,7 +112,7 @@ module key_expansion(
 				.addr(rcon_addr),
 				.dout(rcon_out));	
 	
-	key_expansion_logic u_key_expansion_logic(
+	key_expansion_pipeline(
 					.clk(clk),
 					.rst(rst),
 					.load_enable(load_enable),
